3bb017a9770a76db39496c42234ae0416b10898e
0codinga <rankedkid@gmail.com>
2021-12-23 14:08:38 +0300

    Deploy to Heroku

--- learning/settings.py
+++ learning/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for learning project.
 
@@ -9,9 +10,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_heroku
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-django_heroku.settings(locals())
 
 from pathlib import Path
 
@@ -125,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+django_heroku.settings(locals())
